c39d5181f47a224f18b55bb925ff7145047c923b
BOUILLAGUET Quentin <quentin.bouillaguet@ign.fr>
2026-01-07 16:22:26 +0100

    gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://desplandis.github.io/astro-website/',
+  site: 'https://desplandis.github.io/',
+  base: '/astro-website',
   output: 'static',
   integrations: [mdx()],
   markdown: {
